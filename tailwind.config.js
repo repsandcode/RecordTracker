30b3f69ee69167aafed16f48469195c949bdc83a
Albert Jonathan <borjajonathann@gmail.com>
2022-09-26 09:53:14 +0800

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
+    './views/index.ejs',
+    './views/login.ejs',
+    './views/signup.ejs',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  // tailwind.config.js
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+  ],
 }
