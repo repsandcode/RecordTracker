43a3a585430f0b887803de6ffed1e1aa8824c29c
Albert Jonathan <borjajonathann@gmail.com>
2022-10-02 21:36:40 +0800

    added and modified

--- models/Post.js
+++ models/Post.js
@@ -1,29 +1,73 @@
 const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-  name: {
+  initialCallSetting: {
+    type: String,
+    required: true,
+    enum: ['Door to Door Ministry', 'Street Witnessing', 'Public Witnessing', 'Informal Witnessing', 'Referral']
+  },
+  completeName: {
     type: String,
     required: true,
   },
-  image: {
-    type: String,
-    require: true,
-  },
-  cloudinaryId: {
-    type: String,
-    require: true,
-  },
-  caption: {
+  nationality: {
     type: String,
     required: true,
   },
-  likes: {
-    type: Number,
+  gender: {
+    type: String,
+    required: true,
+    enum: ['Male', 'Female'],
+  },
+  area: {
+    type: String,
     required: true,
   },
-  user: {
+  houseAdress: {
+    type: String,
+    required: true,
+  },
+  landMark: {
+    type: String,
+  },
+  emailAddress: {
+    type: String,
+  },
+  telephoneNumber: {
+    type: String,
+  },
+  socialMediaLink: {
+    type: String,
+  },
+  publisher: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ['Initial Call', 'Return Visit', 'Bible Study', 'Not Interested', 'Do Not Call', 'Not in Territory'],
+  },
+  comments: {
+    type: String,
+    required: true,
+  },
+  publications: {
+    type: String,
+    required: true,
+  },
+  isEnglishDependent: {
+    type: String,
+    enum: ['Yes', 'No'],
+  },
+  livesWithPangasinan: {
+    type: String,
+    enum: ['Yes', 'No'],
+  },
+  isAttendingMeetings: {
+    type: String,
+    enum: ['Yes', 'No'],
   },
   createdAt: {
     type: Date,
--- routes/main.js
+++ routes/main.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const homeController = require('../controllers/home');
 const authController = require('../controllers/auth');
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 // Main Routes - simplified for now
 router.get("/", homeController.getIndex);
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignup);
+router.get("/feed", ensureAuth, postsController.getFeed);
 
 module.exports = router;
